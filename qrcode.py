93c7d6aac66a71e01c3524099e2bb9b89501e58b
nikhil kori <nikhilkori88155@gmail.com>
2025-05-26 11:15:44 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,22 +1,18 @@
 import qrcode
-# taking upi id as a input
+
 upi_id = input("enter your upi id = ")
 
-#upi: //pay?pa=upi_id&apn=name&am=amount&cu=CURRENCY&tn=MESSAGE
 
-#defining the payment URL based on the UPI ID  the payment app 
-# you can modify URLs based on the payment apps you want to support
 
 phonepe_url = f'upi://pay?pa={upi_id}&pn=recipient%20name&mc=1234'
 paytm_url = f'upi://pay?pa={upi_id}&pn=recipient%20name&mc=1234'
 googlepay_url = f'upi://pay?pa={upi_id}&pn=recipient%20name&mc=1234'
 
-#create qrcode each payment app 
 phonepe_qr = qrcode.make(phonepe_url)
 paytm_qr = qrcode.make(paytm_url)
 google_pay_qr = qrcode.make(google_pay_url)
 
-# save the qrcode to image file (optional)
+
 phonepe_qr.save('phonepe_qr.png')
 paytm_qr.save('paytm_qr.png')
 google_pay_qr.save('google_pay_qr.png')
